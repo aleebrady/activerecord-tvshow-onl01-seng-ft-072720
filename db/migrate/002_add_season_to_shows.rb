44af729345deff55cfbe9d5aa41f69aee30e0b43
Aaron Brady <aleebrady@gmail.com>
2020-09-02 05:58:00 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,8 +1,7 @@
-lass CreateShows < ActiveRecord::Migration[5.2]
+class AddSeasonToShows < ActiveRecord::Migration[5.2]
   
   def change 
-    
+    add_column :shows, :season, :string
   end
-  
   
 end
