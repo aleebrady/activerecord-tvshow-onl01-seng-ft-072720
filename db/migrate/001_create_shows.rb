2e27f3e2c19547e4e8ea63d9398bea00582ea223
Aaron Brady <aleebrady@gmail.com>
2020-09-02 05:48:32 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -2,7 +2,11 @@ class CreateShows < ActiveRecord::Migration[5.2]
   
   def change 
     create_table :shows do |t|
-      t. 
+      t.string :name 
+      t.string :network
+      t.string :day 
+      t.integer :rating 
+    end
   end
   
   
